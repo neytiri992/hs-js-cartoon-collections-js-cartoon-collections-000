886cfc4a6e8ce8ab0e089022f6bf09704f6feb28
Katarina Kisin <rina_inna@hotmail.com>
2019-09-17 18:05:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function dwarfRollCall(dwarves) {
+  var dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  var array = []
+for (let i = 0; i < dwarfs.length; i++) {
+  array.push(i+1 + '. ' + dwarfs[i])
+}
+console.log(array.join(', '))
+return array
 }
 
 function summonCaptainPlanet(planeteerCalls){
